b650e072c0d79cedbd42f6b7330287b565a63b4f
chenyang2 <chenyang2@hxdi.com>
2024-03-04 09:53:30 +0800

    feat: vue不再external，使用该组件的工程，不需要再对vue、vue-renderer单独做external处理

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import { Suspense } from 'vue'
+import { Suspense } from 'vue/dist/vue.esm-browser'
 const packages = [{ package: 'lowcode-material-ant-vue', version: '0.1.1', library: 'LowcodeMaterialAntVue', urls: ['http://10.13.4.153:9000/lowcode-resource/lowcode-material-ant-vue_v0.1.1/index.css', 'http://10.13.4.153:9000/lowcode-resource/lowcode-material-ant-vue_v0.1.1/index.js'] }]
 const projectSchema = ProjectSchema
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,16 @@
 import LowCodePreview from './LowCodeSchemaComp.vue';
+import * as vue from 'vue/dist/vue.cjs'
+// const vue = require('vue/dist/vue.esm-browser');
+import VueRenderer from '@knxcloud/lowcode-vue-renderer';
 
 const install = (App) => {
+	window.Vue = vue
+	window.LCVueRenderer = VueRenderer;
 	console.log('window.Vue', window.Vue);
-	App.component(LowCodePreview.name, LowCodePreview);
-};
+	console.log('window.LCVueRenderer', window.LCVueRenderer);
 
+	App.component(LowCodePreview.name, LowCodePreview);
+}
 
 // let windowObj = window;
 // /* 支持使用标签的方式引入 */
